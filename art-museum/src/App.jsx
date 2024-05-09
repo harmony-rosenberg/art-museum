c2ba468e0ad90da1ac87c90246831ec5ff88f22f
harmony <harmony.rosenberg94@gmail.com>
2024-05-09 17:54:18 -0500

    project setup

---
+++ art-museum/src/App.jsx
@@ -0,0 +1,17 @@
+import {createBrowserRouter, RouterProvider } from 'react-router-dom';
+import harvardArt from './data/harvardArt';
+
+console.log(harvardArt)
+
+const router = createBrowserRouter([
+  {
+    path: "*",
+    element: <h2>Page Not Found</h2>
+  },
+])
+
+function App() {
+  return <RouterProvider router={router} />
+}
+
+export default App;
