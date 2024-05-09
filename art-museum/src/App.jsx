7ca42dda0f1c4d76d7642e18a0e0ace61d4dfe01
harmony <harmony.rosenberg94@gmail.com>
2024-05-09 17:58:24 -0500

    phase-1

--- art-museum/src/App.jsx
+++ art-museum/src/App.jsx
@@ -8,6 +8,13 @@ const router = createBrowserRouter([
     path: "*",
     element: <h2>Page Not Found</h2>
   },
+  {
+    path: '/',
+    element: <>
+    <h2>Harvard Art Museum</h2>
+    <p>Look, but Don't Touch. Please select a Gallery in the navigation bar.</p>
+    </>
+  }
 ])
 
 function App() {
